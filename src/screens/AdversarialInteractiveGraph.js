bf6c770fe3370e2187ec32eb201fada9a263d13c
NelsonWang <wangnelson2@gmail.com>
2024-05-14 15:55:49 -0400

    alpha beta

--- src/screens/AdversarialInteractiveGraph.js
+++ src/screens/AdversarialInteractiveGraph.js
@@ -51,13 +51,13 @@ function preComputeAlphaBeta(nodes, edges, { verbose = false } = {}) {
                 table: getTable(value, alpha, beta, maximizingPlayer, message),
             });
         }
-        
+
         recordHistory(nodeId, highlightedNodes, highlightedEdges, value, alpha, beta, maximizingPlayer, 'Traversing Node');
         if (maximizingPlayer) {
             value = -Infinity;
             for (const edge of nodeEdges) {
                 highlightedEdges.push(edge.id);
-                
+
                 const result = alphaBeta(edge.target, alpha, beta, false, edge);
                 value = Math.max(value, result);
                 alpha = Math.max(alpha, value);
@@ -152,11 +152,67 @@ const exampleDatasets = [
             { id: 'GH -> H', source: 'GH', target: 'H', label: '> H' }
         ],
         desc_img: [
-            'https://www.webpagescreenshot.info/image-url/ArWcsfuwE'
+            'https://www.webpagescreenshot.info/image-url/KtcW8brny'
         ],
-        desc:[
+        desc: [
             'Figure 1: https://www.mygreatlearning.com/blog/alpha-beta-pruning-in-ai/'
         ]
+    },
+    {
+        name: "distinct_multiple_children",
+        // A2 B7 C15 D2 E7 F9 G4 H4 I10 J11 H2 L15 M7
+        nodes: [
+            { id: 'ABCDEFGHIJKLM', label: 'ABCDEFGHIJKLM', value: null },
+            { id: 'ABCD', label: 'ABCD', value: null },
+            { id: 'EFGH', label: 'EFGH', value: null },
+            { id: 'IJKLM', label: 'IJKLM', value: null },
+            { id: 'AB', label: 'AB', value: null },
+            { id: 'FG', lFGel: 'AB', value: null },
+            { id: 'IJ', label: 'IJ', value: null },
+            { id: 'KLM', label: 'KLM', value: null },
+            { id: 'A', label: 'A', value: 2 },
+            { id: 'B', label: 'B', value: 7 },
+            { id: 'c', label: 'C', value: 15 },
+            { id: 'D', label: 'D', value: 2 },
+            { id: 'E', label: 'E', value: 7 },
+            { id: 'F', label: 'F', value: 9 },
+            { id: 'G', label: 'G', value: 4 },
+            { id: 'H', label: 'H', value: 4 },
+            { id: 'I', label: 'I', value: 10 },
+            { id: 'J', label: 'J', value: 11 },
+            { id: 'K', label: 'K', value: 2 },
+            { id: 'L', label: 'L', value: 15 },
+            { id: 'M', label: 'M', value: 7 },
+        ],
+        edges: [
+            { id: 'ABCDEFGHIJKLM -> ABCD', source: 'ABCDEFGHIJKLM', target: 'ABCD', label: '> ABCD' },
+            { id: 'ABCDEFGHIJKLM -> EFGH', source: 'ABCDEFGHIJKLM', target: 'EFGH', label: '> EFGH' },
+            { id: 'ABCDEFGHIJKLM -> IJKLM', source: 'ABCDEFGHIJKLM', target: 'IJKLM', label: '> IJKLM' },
+            {id: 'ABCD -> AB', source: 'ABCD', target: 'AB', label: '> AB' },
+            {id: 'ABCD -> IJ', source: 'ABCD', target: 'IJ', label: '> IJ' },
+            {id: 'IJKLM -> KLM', source: 'IJKLM', target: 'KLM', label: '> KLM' },
+            {id: 'ABCD->C', source: 'ABCD', target: 'C', label: '> C'},
+            {id: 'ABCD->D', source: 'ABCD', target: 'D', label: '> D'},
+            {id: 'EFGH->E', source: 'EFGH', target: 'E', label: '> E'},
+            {id: 'EFGH->H', source: 'EFGH', target: 'H', label: '> H'},
+            
+            {id: 'IJ->I', source: 'IJ', target: 'I', label: '> I'},
+            {id: 'IJ->J', source: 'IJ', target: 'J', label: '> J'},
+            {id: 'KLM->K', source: 'KLM', target: 'K', label: '> K'},
+            {id: 'KLM->L', source: 'KLM', target: 'L', label: '> L'},
+            {id: 'KLM->M', source: 'KLM', target: 'M', label: '> M'},
+            {id: 'AB->A', source: 'AB', target: 'A', label: '> A'},
+            {id: 'AB->B', source: 'AB', target: 'B', label: '> B'},
+            {id: 'FG->F', source: 'FG', target: 'F', label: '> F'},
+            {id: 'FG->G', source: 'FG', target: 'G', label: '> G'},
+        ],
+        desc_img: [
+            'https://ctrl.vi/i/KtcW8brny'
+        ],
+        desc: [
+            'Figure 1: https://stackoverflow.com/questions/59974673/minimax-question-regarding-alpha-beta-pruning'
+        ]
+
     }
 ];
 
